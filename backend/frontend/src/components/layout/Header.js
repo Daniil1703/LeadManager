a4601e662eb14b359f90d24cd6994cd84e4655b0
Daniil1703 <hlistunovd@yandex.ru>
2020-05-07 17:20:44 +0300

    Download React application

---
+++ backend/frontend/src/components/layout/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Header extends Component {
+    render(){
+        return(
+            <nav className="nev" >
+                <a className="dev" href="/">LEAD MANAGER</a>
+
+            </nav>
+        )
+    }
+}
+
+export default Header
